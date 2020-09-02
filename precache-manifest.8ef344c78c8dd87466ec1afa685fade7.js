2001cf7faacdbb9759af50adb528f14df90d3b08
Ramsey <Ramvandermeer@gmail.com>
2020-09-01 18:34:48 -0700

    Updates

--- precache-manifest.8ef344c78c8dd87466ec1afa685fade7.js
+++ precache-manifest.8ef344c78c8dd87466ec1afa685fade7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8cb8f4b0e17809ae76d979c352fbbe5",
+    "revision": "ea135c860d91898e5663b546c35dde80",
     "url": "/index.html"
   },
   {
-    "revision": "a13c238dfa3dc38cc351",
+    "revision": "61871539077d6fd9b8c0",
     "url": "/static/css/main.b9e9cb0c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.66e44d9b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a13c238dfa3dc38cc351",
-    "url": "/static/js/main.5e4484d0.chunk.js"
+    "revision": "61871539077d6fd9b8c0",
+    "url": "/static/js/main.15f5dcca.chunk.js"
   },
   {
     "revision": "f57bd6b3cd488554ebce",
